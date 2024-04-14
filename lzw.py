f09380f2c706400604ed69029f7c554bceb794c2
guilherme-iram <guilherme.iram@gmail.com>
2024-04-14 17:11:00 -0300

    Decode com 9 bits

--- lzw.py
+++ lzw.py
@@ -3,7 +3,7 @@ import sys
 from sys import argv
 from struct import *
 
-file_path = "abracadabra"
+file_path = "xml"
 
 with open(file_path, "rb") as file:
     
@@ -11,75 +11,86 @@ with open(file_path, "rb") as file:
 
     # max size = 4096, 32768, 262144, 2097152
     maximum_table_size = 4096
-    print(maximum_table_size)
+    # print(maximum_table_size)
     dictionary_size = 256
 
     dictionary = {chr(i): i for i in range(dictionary_size)}    
     string = ""             # String is null.
-    compressed_data = []    # variable to store the compressed data.
 
     length = len(silesia_concat)
     # add EOF to the end of the file
     # silesia_concat += b'EOF'
+    # print(silesia_concat[:1])
+    all_bytes = []
+    all_bytes_str = []
 
     for i, symbol in enumerate(silesia_concat):
         
+        if i == 9:
+            print("qualquer coisa")
         symbol = chr(symbol)
         
         if (i % 100000 == 0):
             print(f"Progress encoding: {i / length * 100:.2f}%")
 
-        print(symbol)
+        # print(symbol)
         string_plus_symbol = string + symbol # get input symbol.
         
         if string_plus_symbol in dictionary: 
             string = string_plus_symbol
             # condition to get the EOF
-            if i == length - 1:
-                compressed_data.append(dictionary[string])
+            
         else:
-            #length -= len(string)
-            compressed_data.append(dictionary[string])
-            if(len(dictionary) <= maximum_table_size):
+
+            p = (dictionary_size).bit_length()
+            all_bytes.append(format(dictionary[string], f'0{p}b'))
+            all_bytes_str.append(string)
+            if(len(dictionary) < maximum_table_size):
                 dictionary[string_plus_symbol] = dictionary_size
                 dictionary_size += 1
+            
             string = symbol
             
-        
-    print(compressed_data)
-    print(dictionary)
+        if i == length - 1:
+            p = (dictionary_size).bit_length()
+            all_bytes.append(format(dictionary[string], f'0{p}b'))
+            all_bytes_str.append(string)
+    # print(compressed_data)
+    # print(dictionary)
 
     output_file = open(file_path + ".lzw", "wb")
     
-    all_bytes = []
 
-    print("Making bytes: ")
-    for i, data in enumerate(compressed_data):
-        if (i % 100000 == 0):
-            print(f"Progress bytes: {i / len(compressed_data) * 100:.2f}%")
+
+    # print("Making bytes: ")
+    # for i, data in enumerate(compressed_data):
+    #     if (i % 100000 == 0):
+    #         print(f"Progress bytes: {i / len(compressed_data) * 100:.2f}%")
         
-        p = data.bit_length()
-        if p < 8:
-            p = 8
-        print(data, ": ", p, " - bin: ", format(data, f'0{p}b'))
-        all_bytes.append(format(data, f'0{p}b'))
-    
+    #     p = data.bit_length()
+    #     if p < 8:
+    #         p = 8
+    #     # print(data, ": ", p, " - bin: ", format(data, f'0{p}b'))
+    #     all_bytes.append(format(data, f'0{p}b'))
+    #     # all_bytes.append(format(data, f"{0:p}b"))
+
     current_bytes = ""
 
-    print(all_bytes)
     print("Writing binary: ")
     for i, byte in enumerate(all_bytes):
 
-        if (i % 100000 == 0):
-            print(f"Progress write binary: {i / len(compressed_data) * 100:.2f}%")
+        # if (i % 100000 == 0):
+          #   print(f"Progress write binary: {i / len(compressed_data) * 100:.2f}%")
 
         current_bytes += byte
-        print(current_bytes)
+
         while len(current_bytes) >= 8:
+            
             bits_to_write = current_bytes[:8]
             to_write = int(bits_to_write, 2)
             output_file.write(pack('B', to_write))
             current_bytes = current_bytes[8:]
+
             if i == len(all_bytes) - 1:
                 diff = 8 - len(current_bytes)
                 to_write_eof =  ('0' * diff) + current_bytes
--- lzw_decode.py
+++ lzw_decode.py
@@ -3,7 +3,7 @@ import sys
 from sys import argv
 from struct import *
 
-file_path = "abracadabra.lzw"
+file_path = "xml.lzw"
 
 with open(file_path, "rb") as file:
     
@@ -16,7 +16,7 @@ dictionary_size = 256
 
 dictionary = {i:chr(i) for i in range(dictionary_size)}    
 string = ""             # String is null.
-compressed_data = []    # variable to store the compressed data.
+descompressed_data = ""    # variable to store the compressed data.
 
 length = len(silesia_concat)
 # add EOF to the end of the file
@@ -24,91 +24,52 @@ length = len(silesia_concat)
 
 todos_os_bytes = ''
 for i, symbol in enumerate(silesia_concat):
-    print(symbol)
-    
+    if (i % 100000 == 0):
+        print(f"Progress reading: {i / length * 100:.2f}%")    
     todos_os_bytes += format(symbol, '08b')
-    
-    
-    symbol = chr(symbol)
 
-print(todos_os_bytes)   
-
-string_bit = b'0'
+# string_bit = b'0'
 c = 8
 j = 0
-
+string = ""
+p = 8
 while True:
-    symbol_bit = int(todos_os_bytes[:c], 2)
-    
-    symbol = dictionary[symbol_bit]
-    # if j > 0:
-    #     ch1 = dictionary[int(string, 2)] # get the string value from the dictionary
-    # ch2 = dictionary[int(symbol, 2)] # get the symbol value from the dictionary
-    string_plus_symbol = string + symbol # get input symbol.
-    string_plus_symbol_bit = bin(int(string_bit, 2) + symbol_bit)
 
-    if (i % 100000 == 0):
-        print(f"Progress encoding: {i / length * 100:.2f}%")
+    p = (dictionary_size).bit_length()
+    # all_bytes.append(format(dictionary[string], f'0{p}b'))
+
+    key_dict = int(todos_os_bytes[:p], 2)
+    
+    symbol = dictionary[key_dict]
+
+    string_plus_symbol = string + symbol
 
     if string_plus_symbol in dictionary.values(): 
+        descompressed_data += dictionary[key_dict]
         string = string_plus_symbol
-        string_bit = string_plus_symbol_bit
+
         # condition to get the EOF
         # if i == length - 1:
-        #     compressed_data.append(dictionary[string])
+          #   compressed_data.append(dictionary[string])
     else:
         #length -= len(string)
-        compressed_data.append(dictionary[int(string_bit, 2)])
+        descompressed_data += dictionary[key_dict]
         if(len(dictionary) <= maximum_table_size):
             dictionary[dictionary_size] = string_plus_symbol
             dictionary_size += 1
         string = symbol
-        string_bit = bin(symbol_bit)
-        
-    c = int(string_plus_symbol_bit, 2).bit_length()
-    
-    if c < 8:
-        c = 8
-    j += 1
-    
-    todos_os_bytes = todos_os_bytes[c:]
-        
-    # print(compressed_data)
-    # print(dictionary)
 
-    # output_file = open(file_path + ".lzw", "wb")
-    
-    # all_bytes = []
+    todos_os_bytes = todos_os_bytes[p:]
+    print("len:", len(todos_os_bytes))
+    if (len(todos_os_bytes) < 8):
+        break
 
-    # print("Making bytes: ")
-    # for i, data in enumerate(compressed_data):
-    #     if (i % 100000 == 0):
-    #         print(f"Progress bytes: {i / len(compressed_data) * 100:.2f}%")
-        
-    #     p = data.bit_length() + 1
-    #     all_bytes.append(format(data, f'0{p}b'))
-    
-    # current_bytes = ""
 
-    # print(all_bytes)
-    # print("Writing binary: ")
-    # for i, byte in enumerate(all_bytes):
+output_file = open(file_path.split('.')[0] + "_descompressed.lzw", "wb")
+for i, char in enumerate(descompressed_data):
+    if (i % 100000 == 0):
+        print(f"Progress write binary: {i / len(descompressed_data) * 100:.2f}%")
 
-    #     if (i % 100000 == 0):
-    #         print(f"Progress write binary: {i / len(compressed_data) * 100:.2f}%")
+    output_file.write(pack('B', ord(char)))
 
-    #     current_bytes += byte
-    #     print(current_bytes)
-    #     while len(current_bytes) >= 8:
-    #         bits_to_write = current_bytes[:8]
-    #         to_write = int(bits_to_write, 2)
-    #         output_file.write(pack('B', to_write))
-    #         current_bytes = current_bytes[8:]
-    #         if i == len(all_bytes) - 1:
-    #             diff = 8 - len(current_bytes)
-    #             to_write_eof =  ('0' * diff) + current_bytes
-    #             output_file.write(pack('B', int(to_write_eof, 2)))
-                
-            
-    # output_file.close()
-    # file.close()
+output_file.close()
