9dd3bec7c3c3f3b56f77e93d63a7a1d73bf1d8b1
luiz826 <luiz.costa@academico.ufpb.br>
2024-04-19 20:57:07 -0300

    with dict limit

--- lzw_new_approach.py
+++ lzw_new_approach.py
@@ -1,9 +1,10 @@
 from typing import Dict, List, Tuple
 from io import TextIOWrapper
 from struct import pack
+import numpy as np
+import matplotlib.pyplot as plt
 
 ONE_BYTE = 8
-
 
 class IOHandler:
     def __init__(self, path) -> None:
@@ -16,26 +17,33 @@ class IOHandler:
             file = f.read()
         return file
 
-    def write_file_to_encode(self, encoded_message: List[str]) -> None:
+    def write_file_to_encode(self, encoded_message):
         output_file: TextIOWrapper = open(self.write_path, "wb")
+        
         i = 0
-        while len(encoded_message) > ONE_BYTE:
-            bits_to_write = encoded_message[:ONE_BYTE]
+        print("START - WRITE FILE TO ENCODE")
+        
+        current_bytes = encoded_message.pop(0)
+
+        while len(encoded_message) > 0:
+
+            if len(current_bytes) < ONE_BYTE:
+                current_bytes += encoded_message.pop(0) 
+
+            bits_to_write = current_bytes[:ONE_BYTE]
             to_write = int(bits_to_write, 2)
             output_file.write(pack("B", to_write))
-            encoded_message = encoded_message[ONE_BYTE:]
-            print(i)
+            current_bytes = current_bytes[ONE_BYTE:]
 
-            i += 1
-
-        if len(encoded_message) > 0:
-            bits_to_write = encoded_message
+        if len(current_bytes) > 0:
+            bits_to_write = current_bytes
             diff = ONE_BYTE - len(bits_to_write)
             bits_to_write = bits_to_write + ("0" * diff)
             to_write = int(bits_to_write, 2)
             output_file.write(pack("B", to_write))
 
         output_file.close()
+        print("DONE - WRITE FILE TO ENCODE")
 
     def read_file_to_decode(self) -> str:
         with open(self.write_path, "rb") as f:
@@ -46,45 +54,66 @@ class IOHandler:
         with open(self.write_decoded_path, "wb") as f:
             last_char = len(decoded_message) - 1
             for i, bits in enumerate(decoded_message):
-                # corner case last char 
-                if i != last_char:
-                    f.write(pack("B", ord(bits)))
-        
+                # if i != last_char:
+                f.write(pack("B", ord(bits)))
 
 
 def to_bin(integer: int, number_of_bits: int) -> str:
     return format(integer, f"0{number_of_bits}b")
+
 
 def to_int(bin_formated: str) -> int:
     return int(bin_formated, 2)
 
 
 class LZW:
+
+    def __init__(self, maximum_table_size: int, dict_strategy: int) -> None:
+        self.maximum_table_size = maximum_table_size
+        self.dict_strategy = dict_strategy
+
     def encode(self, message: str) -> Tuple[str, List[int]]:
         dict_size: int = 255
         dictionary: dict = {chr(i): i for i in range(dict_size + 1)}
 
         found_chars: str = ""
-        result: str = ""
+        result = []
         bits_of_each_symbol: List[int] = []
-        for char in message:
+        
+        moving_avg_list = []
+        sum_bit = 8
+
+        str_debug = ""
+
+        for i, char in enumerate(message):
             dict_bits_size: int = dict_size.bit_length()
             char = chr(char)
             chars_to_add: str = found_chars + char
             if chars_to_add in dictionary:
                 found_chars = chars_to_add
             else:
-                result += to_bin(dictionary[found_chars], dict_bits_size)
+                sum_bit += dict_bits_size
+                result.append(to_bin(dictionary[found_chars], dict_bits_size))
+                str_debug += found_chars
                 bits_of_each_symbol.append(dict_bits_size)
-                dict_size += 1
-                dictionary[chars_to_add] = dict_size
+
+                if dict_size < self.maximum_table_size:
+                    dict_size += 1
+                    dictionary[chars_to_add] = dict_size
+                
                 found_chars = char
+                
+
+            moving_avg_list.append(sum_bit / (i + 1))
 
         if len(found_chars) > 0:
-            result += to_bin(dictionary[found_chars], dict_bits_size)
+            result.append(to_bin(dictionary[found_chars], dict_bits_size))
+            str_debug += found_chars
             bits_of_each_symbol.append(dict_bits_size)
 
-        return result, bits_of_each_symbol
+        print(dict_size)
+
+        return result, bits_of_each_symbol, moving_avg_list
 
     def decode(self, encoded_message: List[str]) -> str:
         dict_size: int = 256
@@ -92,8 +121,8 @@ class LZW:
 
         chars = chr(to_int(encoded_message[0]))
         result = chars
-        encoded_message.pop(0)
 
+        encoded_message.pop(0)
         encoded_message_in_bits: str = "".join(encoded_message)
 
         while len(encoded_message_in_bits) > 0:
@@ -103,23 +132,35 @@ class LZW:
             entry: str = dictionary[code] if code in dictionary else chars + chars[0]
             result += entry
 
-            dictionary[dict_size] = chars + entry[0]
-            dict_size += 1
+            if dict_size < (self.maximum_table_size + 2):
+                dictionary[dict_size] = chars + entry[0]
+                dict_size += 1
             chars = entry
 
             encoded_message_in_bits = encoded_message_in_bits[dict_bits_size:]
 
+        print(dict_size)
+
         return result
 
+MAX_LEN_DICT = [4096, 32768, 262144, 2097152]
 
-l = LZW()
-io = IOHandler("xml")
+l = LZW(MAX_LEN_DICT[0], 1)
+io = IOHandler("republic")
 m = io.read_file_to_encode()
-r, b = l.encode(m)
+r, b, moving_avg_list = l.encode(m)
+
 io.write_file_to_encode(r)
+print("Lendo arquivo codificado")
 r_e = io.read_file_to_decode()
+print("Decodificando ...")
 r_d = l.decode(r_e)
+print("Escrevendo arquivo decodificado...")
 io.write_file_to_decode(r_d)
+print("FIM")
+
+# plt.plot(moving_avg_list)
+# plt.show()
 
 # original = ""
 # has_error = False
