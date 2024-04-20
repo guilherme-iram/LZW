67acc82e4ddeabc2a949628d85bf1c4161f5b130
luiz826 <luiz.costa@academico.ufpb.br>
2024-04-20 00:51:19 -0300

    Add table making

--- lzw_new_approach.py
+++ lzw_new_approach.py
@@ -3,15 +3,19 @@ from io import TextIOWrapper
 from struct import pack
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd 
 import time
+import os
 
 ONE_BYTE = 8
-
 class IOHandler:
-    def __init__(self, path) -> None:
-        self.read_path = f"./data_test/{path}"
-        self.write_path = f"./data_test/{path}.lzw"
-        self.write_decoded_path = f"./data_test/{path}.lzw.decoded"
+    def __init__(self, path, debug_mode=True, sufix="") -> None:
+        
+        prefix = "data_test" if debug_mode else "data/silesia_corpus"
+        
+        self.read_path = f"./{prefix}/{path}"
+        self.write_path = f"./{prefix}/{path}{sufix}.lzw"
+        self.write_decoded_path = f"./{prefix}/{path}{sufix}.lzw.decoded"
 
     def read_file_to_encode(self) -> str:
         with open(self.read_path, "rb") as f:
@@ -21,11 +25,7 @@ class IOHandler:
     def write_file_to_encode(self, encoded_message):
         output_file: TextIOWrapper = open(self.write_path, "wb")
         
-        i = 0
         print("START - WRITE FILE TO ENCODE")
-        
-        # current_bytes = encoded_message.pop(0)
-        
         bits_array = "".join(encoded_message)
 
         while len(bits_array) >= ONE_BYTE:
@@ -51,11 +51,9 @@ class IOHandler:
     
     def write_file_to_decode(self, decoded_message: str) -> None:
         with open(self.write_decoded_path, "wb") as f:
-            last_char = len(decoded_message) - 1
             for i, bits in enumerate(decoded_message):
-                # if i != last_char:
                 f.write(pack("B", ord(bits)))
-
+                
 
 def to_bin(integer: int, number_of_bits: int) -> str:
     return format(integer, f"0{number_of_bits}b")
@@ -64,25 +62,38 @@ def to_bin(integer: int, number_of_bits: int) -> str:
 def to_int(bin_formated: str) -> int:
     return int(bin_formated, 2)
 
-
 class LZW:
     def __init__(self, maximum_table_size: int, dict_strategy: int) -> None:
         self.maximum_table_size = maximum_table_size
         self.dict_strategy = dict_strategy
 
+    def _set_dict_encode(self, dict_size: int) -> dict:
+        return {chr(i): i for i in range(dict_size + 1)}
+    
+    def _set_dict_decode(self, dict_size: int) -> dict:
+        return {i: chr(i) for i in range(dict_size)}
+    
+    def _is_descending(self, moving_avg_list: List[float], threshold: int = 30) -> bool:
+        count_descending = 0
+        count_stable_or_ascending = 0
+        for i in range(len(moving_avg_list) - np.min(threshold, len(moving_avg_list)), len(moving_avg_list) - 1):
+            diff = moving_avg_list[i] > moving_avg_list[i + 1]
+            if diff:
+                count_descending += 1
+            else:
+                count_stable_or_ascending += 1  
+        
+        return count_descending > count_stable_or_ascending
+    
     def encode(self, message: str) -> Tuple[str, List[int]]:
         dict_size: int = 255
-        dictionary: dict = {chr(i): i for i in range(dict_size + 1)}
+        dictionary: dict = self._set_dict_encode(dict_size)
 
         found_chars: str = ""
         result = []
-        bits_of_each_symbol: List[int] = []
         
         moving_avg_list = []
-        sum_bit = 8
-
-        str_debug = ""
-        n = []
+        sum_bit = ONE_BYTE
 
         for i, char in enumerate(message):
             dict_bits_size: int = dict_size.bit_length()
@@ -92,132 +103,130 @@ class LZW:
                 found_chars = chars_to_add
             else:
                 sum_bit += dict_bits_size
-                n.append(dictionary[found_chars])
                 result.append(to_bin(dictionary[found_chars], dict_bits_size))
-                str_debug += found_chars
-                bits_of_each_symbol.append(dict_bits_size)
 
                 if dict_size < self.maximum_table_size:
                     dict_size += 1
                     dictionary[chars_to_add] = dict_size
+                else:
+                    if self.dict_strategy == 2:    
+                        dict_size: int = 255
+                        dictionary: dict = self._set_dict_encode(dict_size)
+                    elif self.dict_strategy == 3:
+                        if self._is_descending(moving_avg_list):
+                            dict_size: int = 255
+                            dictionary: dict = self._set_dict_encode(dict_size)
+                        
                 found_chars = char
                 
             moving_avg_list.append(sum_bit / (i + 1))
 
         if len(found_chars) > 0:
-            n.append(dictionary[found_chars])
             result.append(to_bin(dictionary[found_chars], dict_bits_size))
-            str_debug += found_chars
-            bits_of_each_symbol.append(dict_bits_size)
-
-        print(dict_size)
-        with open("debug_enco.txt", "w") as f:
-            f.write(str(dictionary))
-        with open("debug_enco_n.txt", "w") as f:
-            f.write(str(n))
-            
-
-        return result, bits_of_each_symbol, moving_avg_list, dictionary
+    
+        return result, moving_avg_list
 
     def decode(self, encoded_message: List[str]) -> str:
         dict_size: int = 256
-        dictionary: dict = {i: chr(i) for i in range(dict_size)}
+        dictionary: dict = self._set_dict_decode(dict_size)
 
         chars = chr(to_int(encoded_message.pop(0)))
         result = chars
-
+        sum_bit = ONE_BYTE
+        moving_avg_list = []
+        i = 0
         current_decode_bytes = ""
-        i = len(encoded_message)
         while len(encoded_message) > 0:
             dict_bits_size: int = dict_size.bit_length()
             
             while len(current_decode_bytes) < dict_bits_size and len(encoded_message) > 0:
-                i -= 1
                 current_decode_bytes += encoded_message.pop(0)
-
-            if (i == 0):
-                print("Fim do arquivo")
 
             code = to_int(current_decode_bytes[:dict_bits_size])
             current_decode_bytes = current_decode_bytes[dict_bits_size:]
 
             entry: str = dictionary[code] if code in dictionary else chars + chars[0]
             result += entry
-            if (i < 10):
-                print(i)
-                print("=================")
-                print("encoded_message ", encoded_message)
-                print("dict_bits_size ", dict_bits_size)
-                print("current_decode_bytes ", current_decode_bytes)                
-                print("len(encoded_message) ", len(encoded_message))
-                print("code ", code)
-                print("entry", entry)
-                print("=================")
-                
-
+            sum_bit += dict_bits_size
+            
             if dict_size < (self.maximum_table_size + 1):
                 dictionary[dict_size] = chars + entry[0]
                 dict_size += 1
+            else:
+                if self.dict_strategy == 2:    
+                    dict_size: int = 256
+                    dictionary: dict = self._set_dict_decode(dict_size)
+                elif self.dict_strategy == 3:
+                    if self._is_descending(moving_avg_list):
+                        dict_size: int = 256
+                        dictionary: dict = self._set_dict_decode(dict_size)
+                
+            
             chars = entry
-
-        print(dict_size)
-        with open("debug_deco.txt", "w") as f:
-            f.write(str(dictionary))
+            moving_avg_list.append(sum_bit / (i + 1))
+            i += 1
 
         return result
 
 MAX_LEN_DICT = [4096, 32768, 262144, 2097152]
 
-l = LZW(MAX_LEN_DICT[0], 1)
-io = IOHandler("republic")
+strategies = {
+    1: "Sem estratégia",
+    2: "Resetar dicionário",
+    3: "Resetar dicionário se a média móvel for decrescente"
+}
 
-start_encode = time.time()
-m = io.read_file_to_encode()
-r, b, moving_avg_list, dict_ = l.encode(m)
-import pandas as pd 
 
-# ENTROPIA
-P = pd.Series(r).value_counts(normalize=True)
-log2_P = P.apply(lambda x: np.log2(1/x))
-entropy = np.sum(P * log2_P)
-print(entropy)
+def get_entropy(r):
+    P = pd.Series(r).value_counts(normalize=True)
+    log2_P = P.apply(lambda p_i: np.log2(1/p_i))
+    return np.sum(P * log2_P)
 
-with open("r.txt", "w") as f:
-    f.write(str(r))
-io.write_file_to_encode(r)
-end_encode = time.time()
+table_params = {
+    "dict_length": [],
+    "strategy": [],
+    "entropy": [],
+    "compress_rate": [],
+    "encode_time": [],
+    "decode_time": [],
+    "moving_avg_list": []
+}
 
-delta_time_encode = end_encode - start_encode
+def get_compress_rate(encoded_data, original_data):
+    return os.stat(encoded_data).st_size / os.stat(original_data).st_size
 
-print(f"Tempo de execução do encode: {delta_time_encode}")
+for dict_length in MAX_LEN_DICT:
+    for i, strategy_name in strategies.items():
+        io = IOHandler("john_1", debug_mode=True, sufix=f"_{dict_length}_{strategy_name}")
+        l = LZW(dict_length, i)
 
-print("Lendo arquivo codificado")
-start_decode = time.time()
-r_e = io.read_file_to_decode()
-
-with open("r_e", "w") as f:
-    f.write(str(r_e))
-
-print("Decodificando ...")
-r_d = l.decode(r_e)
-print("Escrevendo arquivo decodificado...")
-
-io.write_file_to_decode(r_d)
-end_decode = time.time()
-delta_time_decode = end_decode - start_decode
-print(f"Tempo de execução do encode: {delta_time_decode}")
-print("FIM")
-
-# # plt.plot(moving_avg_list)
-# plt.show()
-
-# original = ""
-# has_error = False
-# for i, j in zip(m, r_d):
-#     original += chr(i)
-#     if chr(i) != j:
-#         print(chr(i), j, chr(i) == j)
-#         has_error = True
-
-# if not has_error:
-#     print("DEU CERTO!")
+        start_encode = time.time()
+        m = io.read_file_to_encode()
+        r, moving_avg_list = l.encode(m)
+        io.write_file_to_encode(r)
+        end_encode = time.time()
+        delta_time_encode = end_encode - start_encode
+        print("Lendo arquivo codificado")
+        start_decode = time.time()
+        r_e = io.read_file_to_decode()
+        print("Decodificando ...")
+        r_d = l.decode(r_e)
+        print("Escrevendo arquivo decodificado...")
+        io.write_file_to_decode(r_d)
+        end_decode = time.time()
+        delta_time_decode = end_decode - start_decode
+        print("FIM")
+        entropy = get_entropy(r)
+        
+        cr = get_compress_rate(io.write_path, io.read_path)
+        
+        table_params["dict_length"].append(dict_length)
+        table_params["strategy"].append(strategy_name)
+        table_params["entropy"].append(entropy)
+        table_params["compress_rate"].append(cr)
+        table_params["encode_time"].append(delta_time_encode)
+        table_params["decode_time"].append(delta_time_decode)
+        table_params["moving_avg_list"].append(moving_avg_list)
+        
+table = pd.DataFrame(table_params)
+table.to_csv("table.csv", index=False)
